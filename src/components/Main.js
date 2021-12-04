61f3c5a59b5b487d2a7d4ae76026897c205ae230
Sarah Edison <edison.sarahc@gmail.com>
2021-12-04 12:02:51 -0500

    feat: set state and api call for profile data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,45 @@
+import {useEffect, useState} from 'react';
 import profileImage from "../images/profile-image-jacques.jpg";
 import PopupWithForm from "./PopupWithForm";
+import Card from './Card';
+import api from "../utils/api"
 
 function Main() {
+
+  const [userName, setUserName] = useState('');
+  const [userDescription, setUserDescription] = useState('');
+  const [userAvatar, setUserAvatar] = useState('')
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    api.getUser()
+  .then(data => {
+   setUserName(data.name)
+   setUserDescription(data.about)
+   setUserAvatar(data.avatar)
+}, [userName, userDescription, userAvatar])
+  })
+
+  useEffect(() => {
+    api.getInitialCards()
+  .then(data => {
+    
+   setCards(data)
+}, [cards])
+  })
+
   const handleEditAvatarClick = () => {
     document.querySelector(".popup_type_avatar").classList.add("popup_opened");
+
+    
   };
   const handleEditProfileClick = () => {
     document.querySelector(".popup_type_edit").classList.add("popup_opened");
+ 
   };
   const handleAddPlaceClick = () => {
     document.querySelector(".popup_type_add").classList.add("popup_opened");
+   
   };
   return (
     <div>
@@ -23,20 +53,20 @@ function Main() {
               onClick={handleEditAvatarClick}
             >
               <img
-                src={profileImage}
+                src={userAvatar}
                 alt="Jacques Cousteau"
                 className="profile__image"
               />
             </button>
             <div className="profile__name-area">
-              <h1 className="profile__name">Sarah</h1>
+              <h1 className="profile__name">{userName}</h1>
               <button
                 aria-label="edit"
                 type="button"
                 className="button profile__edit-button opacity"
                 onClick={handleEditProfileClick}
               ></button>
-              <p className="profile__title">Developer</p>
+              <p className="profile__title">{userDescription}</p>
             </div>
           </div>
           <button
@@ -47,8 +77,15 @@ function Main() {
           ></button>
         </section>
         <section className="images">
-          <ul className="photo-grid"></ul>
+          <ul className="photo-grid">
+            {cards.map((card) => {
+                <Card>
+                name={cards.name}
+                link={cards.link}
+                  </Card>;
+              })} </ul>
         </section>
+  
       </main>
       <PopupWithForm name="edit" title="Edit profile" buttonName="Save">
       <label htmlFor="name" className="form__label">
