98747a51f351163a95e17eb4b900f45db19bc09b
Sarah Edison <edison.sarahc@gmail.com>
2021-12-06 12:19:10 -0500

    feat: add useEffect for card api call. Map over cards array.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,45 +1,55 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 import profileImage from "../images/profile-image-jacques.jpg";
 import PopupWithForm from "./PopupWithForm";
-import Card from './Card';
-import api from "../utils/api"
+import Card from "./Card";
+import api from "../utils/api";
 
 function Main() {
-
-  const [userName, setUserName] = useState('');
-  const [userDescription, setUserDescription] = useState('');
-  const [userAvatar, setUserAvatar] = useState('')
-  const [cards, setCards] = useState([])
-
-  useEffect(() => {
-    api.getUser()
-  .then(data => {
-   setUserName(data.name)
-   setUserDescription(data.about)
-   setUserAvatar(data.avatar)
-}, [userName, userDescription, userAvatar])
-  })
+  const [userName, setUserName] = useState("");
+  const [userDescription, setUserDescription] = useState("");
+  const [userAvatar, setUserAvatar] = useState("");
+  const [cards, setCards] = useState([]);
 
   useEffect(() => {
-    api.getInitialCards()
-  .then(data => {
-    
-   setCards(data)
+    api.getUser().then(
+      (data) => {
+        setUserName(data.name);
+        setUserDescription(data.about);
+        setUserAvatar(data.avatar);
+      }
+    );
+  }, 
+  [userName, userDescription, userAvatar]);
+
+  useEffect(() => {
+    api.getInitialCards().then(
+      (data) => {
+        setCards(
+          data.map(item => (
+            {
+            name: item.name,
+            link: item.link,
+            likes: item.likes.length,
+            id: item._id
+          }
+          ))
+        )
+      });
+  }, 
+[]);
+
+useEffect(() => {
+  console.log(cards)
 }, [cards])
-  })
 
   const handleEditAvatarClick = () => {
     document.querySelector(".popup_type_avatar").classList.add("popup_opened");
-
-    
   };
   const handleEditProfileClick = () => {
     document.querySelector(".popup_type_edit").classList.add("popup_opened");
- 
   };
   const handleAddPlaceClick = () => {
     document.querySelector(".popup_type_add").classList.add("popup_opened");
-   
   };
   return (
     <div>
@@ -78,92 +88,93 @@ function Main() {
         </section>
         <section className="images">
           <ul className="photo-grid">
-            {cards.map((card) => {
-                <Card>
-                name={cards.name}
-                link={cards.link}
-                  </Card>;
-              })} </ul>
+          {cards.map(card => 
+ <Card name={card.name} link={card.link} key={card.id} likes={card.likes}/>
+          )} 
+          </ul>
         </section>
-  
       </main>
       <PopupWithForm name="edit" title="Edit profile" buttonName="Save">
-      <label htmlFor="name" className="form__label">
-      <input
-        type="text"
-        className="form__input form__input_type_name"
-        id="name"
-        name="name"
-        placeholder="Name"
-        minLength="2"
-        maxLength="40"
-        required
-      />
-      <span className="form__error" id="name-error"></span>
-    </label>
-    <label htmlFor="about" className="form__label">
-      <input
-        type="text"
-        className="form__input form__input_type_title"
-        id="about"
-        name="about"
-        placeholder="About me"
-        minLength="2"
-        maxLength="200"
-        required
-      />
-      <span className="form__error" id="about-error"></span>
-    </label></PopupWithForm>
-    
-
-    <PopupWithForm name="add" title="New Place" buttonName="Create">
-    <label htmlFor="name" className="form__label">
-              <input
-                type="text"
-                className="form__input form__input_type_place"
-                id="place-name"
-                name="name"
-                placeholder="Title"
-                minLength="1"
-                maxLength="30"
-                required
-              />
-              <span className="form__error" id="place-name-error"></span>
-            </label>
-            <label htmlFor="link" className="form__label">
-              <input
-                type="url"
-                className="form__input form__input_type_link"
-                id="link"
-                name="link"
-                placeholder="Image link"
-                required
-              />
-              <span className="form__error" id="link-error"></span>
-            </label>
-      </PopupWithForm>
-    
-      <PopupWithForm name="avatar" title="Change profile picture" buttonName="Save">
-      <label htmlFor="link" className="form__label">
-              <input
-                type="url"
-                className="form__input form__input_type_link"
-                id="avatarLink"
-                name="avatarLink"
-                placeholder="Profile image link"
-                required
-              />
-              <span className="form__error" id="avatarLink-error"></span>
-            </label>
+        <label htmlFor="name" className="form__label">
+          <input
+            type="text"
+            className="form__input form__input_type_name"
+            id="name"
+            name="name"
+            placeholder="Name"
+            minLength="2"
+            maxLength="40"
+            required
+          />
+          <span className="form__error" id="name-error"></span>
+        </label>
+        <label htmlFor="about" className="form__label">
+          <input
+            type="text"
+            className="form__input form__input_type_title"
+            id="about"
+            name="about"
+            placeholder="About me"
+            minLength="2"
+            maxLength="200"
+            required
+          />
+          <span className="form__error" id="about-error"></span>
+        </label>
       </PopupWithForm>
 
-      <PopupWithForm name="delete"title="Are you sure?" buttonName="Yes">
-      <input
-              type="hidden"
-              className="form__input form__input_type_card-id"
-              id="id"
-              name="id"
-            />
+      <PopupWithForm name="add" title="New Place" buttonName="Create">
+        <label htmlFor="name" className="form__label">
+          <input
+            type="text"
+            className="form__input form__input_type_place"
+            id="place-name"
+            name="name"
+            placeholder="Title"
+            minLength="1"
+            maxLength="30"
+            required
+          />
+          <span className="form__error" id="place-name-error"></span>
+        </label>
+        <label htmlFor="link" className="form__label">
+          <input
+            type="url"
+            className="form__input form__input_type_link"
+            id="link"
+            name="link"
+            placeholder="Image link"
+            required
+          />
+          <span className="form__error" id="link-error"></span>
+        </label>
+      </PopupWithForm>
+
+      <PopupWithForm
+        name="avatar"
+        title="Change profile picture"
+        buttonName="Save"
+      >
+        <label htmlFor="link" className="form__label">
+          <input
+            type="url"
+            className="form__input form__input_type_link"
+            id="avatarLink"
+            name="avatarLink"
+            placeholder="Profile image link"
+            required
+          />
+          <span className="form__error" id="avatarLink-error"></span>
+        </label>
+      </PopupWithForm>
+
+      <PopupWithForm name="delete" title="Are you sure?" buttonName="Yes">
+        <input
+          type="hidden"
+          className="form__input form__input_type_card-id"
+          id="id"
+          name="id"
+        />
       </PopupWithForm>
     </div>
   );
