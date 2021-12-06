c6bfe236c005caae7e9ed0c1ddf18a9b5a002ad5
Sarah Edison <edison.sarahc@gmail.com>
2021-12-06 12:19:46 -0500

    feat: remove template from Card.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,18 @@
-function Card (props) {
+function Card (card) {
 
 return(
-    <div>
-<template id="cardTemplate" className = "card-template">
+<div>
 <li className="photo-grid__card">
     <button aria-label="delete" type="button" className="photo-grid__delete-icon"></button>
-    <img className="photo-grid__image" alt="" src={props.link}/>
+    <img className="photo-grid__image" alt={card.name} src={card.link}/>
     <div className="photo-grid__content">
-        <h2 className="photo-grid__title">{props.name}</h2>
+        <h2 className="photo-grid__title">{card.name}</h2>
         <div className="photo-grid__likes">
         <button aria-label="like" type="button" id="like" className="photo-grid__heart-icon opacity"></button>
-        <p className="photo-grid__text">{props.name}</p>
+        <p className="photo-grid__text">{card.likes}</p>
     </div>
     </div>
 </li>
-</template> 
 </div>
 )
 }
