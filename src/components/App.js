d14d7d4efff7ea44410fec5e64ee289a4ab3b84d
Sarah Edison <edison.sarahc@gmail.com>
2021-12-06 12:20:14 -0500

    feat: import useEffect and useState add selectedCard const

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
+import {useEffect, useState} from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 
 function App() {
     
+  const [selectedCard, setSelectedCard] = useState('');
+
   return (
     <div className="root">
     <Header/>
