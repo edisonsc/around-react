bfe38d1d16bba6887f6d23195f792a5bcc831f4c
Sarah Edison <edison.sarahc@gmail.com>
2021-12-09 19:41:08 -0500

    feat: Add function handleClick to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,19 @@
-function Card (card) {
+function Card (props) {
+
+    function handleClick() {
+        props.onCardClick(props.card);
+      } 
 
 return(
 <div>
 <li className="photo-grid__card">
     <button aria-label="delete" type="button" className="photo-grid__delete-icon"></button>
-    <img className="photo-grid__image" alt={card.name} src={card.link}/>
+    <img className="photo-grid__image" alt={props.name} src={props.link} onClick={handleClick}/>
     <div className="photo-grid__content">
-        <h2 className="photo-grid__title">{card.name}</h2>
+        <h2 className="photo-grid__title">{props.name}</h2>
         <div className="photo-grid__likes">
         <button aria-label="like" type="button" id="like" className="photo-grid__heart-icon opacity"></button>
-        <p className="photo-grid__text">{card.likes}</p>
+        <p className="photo-grid__text">{props.likes}</p>
     </div>
     </div>
 </li>
