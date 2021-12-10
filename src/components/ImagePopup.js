69cb3b09fdd21234cc24b7db8bf79846451dcc06
Sarah Edison <edison.sarahc@gmail.com>
2021-12-09 19:41:30 -0500

    feat: ImagePopup accepts props and update return statement

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,14 +1,18 @@
-function ImagePopup() {
+function ImagePopup(props) {
+
   return (
-    <div className="popup popup_type_preview">
+    <div
+      className={`popup popup_type_${props.card.name} ${props.card ? "popup_opened" : ""}`}
+    >
       <div className="popup__container popup__container_type_preview">
         <button
           aria-label="close"
           type="button"
           className="popup__close-button popup__close-button_type_preview opacity"
+          onClick={props.onClose}
         ></button>
-        <img src="#" alt="#" className="popup__image" />
-        <h2 className="popup__caption"></h2>
+        <img src={props.card.link} alt={props.card.name} className="popup__image" />
+        <h2 className="popup__caption">{props.card.name}</h2>
       </div>
     </div>
   );
