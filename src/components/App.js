bd9a7e479dce494a18e9c67ec51d0ecc9d1830cd
Sarah Edison <edison.sarahc@gmail.com>
2021-12-09 19:49:55 -0500

    remove: useEffect import for App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import PopupWithForm from "./PopupWithForm";
