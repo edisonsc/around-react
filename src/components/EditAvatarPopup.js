3d5ccf4b9fd7c8fc232e70c3f01769a283bde020
Sarah Edison <edison.sarahc@gmail.com>
2021-12-18 08:25:58 -0500

    feat: aded EditAvatarPopup

---
+++ src/components/EditAvatarPopup.js
@@ -0,0 +1,42 @@
+import PopupWithForm from "./PopupWithForm"
+import React from "react"
+
+function EditAvatarPopup(props){
+
+    const avatarRef = React.useRef();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.onUpdateAvatar({
+          avatar: {avatarRef},
+        });
+     
+      } 
+
+return (
+<PopupWithForm
+        name="avatar"
+        title="Change profile picture"
+        buttonName="Save"
+        isOpen={props.isOpen}
+        onClose={props.onClose}
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="link" className="form__label">
+          <input
+            type="url"
+            className="form__input form__input_type_link"
+            id="avatarLink"
+            name="avatarLink"
+            placeholder="Profile image link"
+            value={avatarRef}
+            // onChange={}
+            required
+          />
+          <span className="form__error" id="avatarLink-error"/>
+        </label>
+      </PopupWithForm>
+)
+}
+
+      export default EditAvatarPopup
