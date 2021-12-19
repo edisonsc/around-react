7ef8cb1daf2a7022f2ee6a5876295c7ff4c31f51
Sarah Edison <edison.sarahc@gmail.com>
2021-12-19 08:26:30 -0500

    feat: add cardButtonDeleteClassName to display delete-icon on owner cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,28 @@
-function Card(props) {
+import React from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import { useContext } from "react";
+
+function Card(card) {
+  const currentUser = useContext(CurrentUserContext);
+
   function handleClick() {
-    props.onCardClick(props.card);
+    card.onCardClick(card);
+    console.log(card)
   }
+
+  // Checking if the current user is the owner of the current card
+  const isOwn = card.owner === currentUser._id;
+
+  // Creating a variable which you'll then set in `className` for the delete button
+  const cardDeleteButtonClassName = `photo-grid__delete-icon ${
+    isOwn ? "photo-grid__delete-icon_visible" : "photo-grid__delete-icon_hidden"
+  }`;
+
+  // Check if the card was liked by the current user
+// const isLiked = card.likes.some(i => i.id === currentUser._id);
+
+// Create a variable which you then set in `className` for the like button
+const cardLikeButtonClassName = `...`; 
 
   return (
     <div>
@@ -9,16 +30,16 @@ function Card(props) {
         <button
           aria-label="delete"
           type="button"
-          className="photo-grid__delete-icon"
+          className={cardDeleteButtonClassName}
         ></button>
         <img
           className="photo-grid__image"
-          alt={props.name}
-          src={props.link}
+          alt={card.name}
+          src={card.link}
           onClick={handleClick}
         />
         <div className="photo-grid__content">
-          <h2 className="photo-grid__title">{props.name}</h2>
+          <h2 className="photo-grid__title">{card.name}</h2>
           <div className="photo-grid__likes">
             <button
               aria-label="like"
@@ -26,7 +47,7 @@ function Card(props) {
               id="like"
               className="photo-grid__heart-icon opacity"
             ></button>
-            <p className="photo-grid__text">{props.likes}</p>
+            <p className="photo-grid__text">{card.likes}</p>
           </div>
         </div>
       </li>
