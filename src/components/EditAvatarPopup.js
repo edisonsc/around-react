ef6b2f675f37ad0f96776cde1be9add32cb04344
Sarah Edison <edison.sarahc@gmail.com>
2021-12-20 09:48:03 -0500

    feat: update EditAvatarPopup with handleChange and handleSubmit functions

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -2,17 +2,19 @@ import PopupWithForm from "./PopupWithForm";
 import React from "react";
 
 function EditAvatarPopup(props) {
-  const [value, setValue] = React.useState("");
-  const avatarRef = React.useRef();
+
+  const [value, setValue] = React.useState('');
+  const avatarRef = React.useRef(null);
 
   function handleChange(e) {
-    setValue(e.target.value);
+   setValue(e.target.value);
   }
   function handleSubmit(e) {
     e.preventDefault();
     props.onUpdateAvatar({
-      avatar: { value },
+      avatar:  value ,
     });
+    setValue('')
   }
 
   return (
@@ -32,9 +34,9 @@ function EditAvatarPopup(props) {
           id="avatarLink"
           name="avatarLink"
           placeholder="Profile image link"
-          value={value}
           onChange={handleChange}
           ref={avatarRef}
+          value= {value}
           required
         />
         <span className="form__error" id="avatarLink-error" />
