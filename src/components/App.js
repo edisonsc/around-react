1a0c4bea89535dd0cbf423a44c5e64c6466c20cf
Sarah Edison <edison.sarahc@gmail.com>
2021-12-20 10:05:45 -0500

    refactor AddPlacePopup to own component

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
 import EditProfilePopup from "./EditProfilePopup";
 import EditAvatarPopup from "./EditAvatarPopup";
+import AddPlacePopup from "./AddPlacePopup";
 import Footer from "./Footer";
 import api from "../utils/api";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
@@ -121,38 +122,11 @@ function App() {
       onUpdateUser={handleUpdateUser}
       />
 
-      <PopupWithForm
-        name="add"
-        title="New Place"
-        buttonName="Create"
-        isOpen={isAddPlacePopupOpen}
-        onClose={closeAllPopups}
-      >
-        <label htmlFor="name" className="form__label">
-          <input
-            type="text"
-            className="form__input form__input_type_place"
-            id="place-name"
-            name="name"
-            placeholder="Title"
-            minLength="1"
-            maxLength="30"
-            required
-          />
-          <span className="form__error" id="place-name-error"/>
-        </label>
-        <label htmlFor="link" className="form__label">
-          <input
-            type="url"
-            className="form__input form__input_type_link"
-            id="link"
-            name="link"
-            placeholder="Image link"
-            required
-          />
-          <span className="form__error" id="link-error"/>
-        </label>
-      </PopupWithForm>
+<AddPlacePopup
+  isOpen={isAddPlacePopupOpen}
+  onClose={closeAllPopups}
+  />
+      
 
       <EditAvatarPopup 
       isOpen={isEditAvatarPopupOpen} 
