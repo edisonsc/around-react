febf6324375549860af5ebfe3487c477344b2a8f
Sarah Edison <edison.sarahc@gmail.com>
2021-12-21 14:02:32 -0500

    remove div tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,8 @@
 function Footer() {
   return (
-    <div>
       <footer className="footer">
         <p className="footer__text">&#169; 2021 Around The U.S.</p>
       </footer>
-    </div>
   );
 }
 
