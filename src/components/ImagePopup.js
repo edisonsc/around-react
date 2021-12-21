7ff4999d538c9e9faf6f7628f3964d73fa15abd8
Sarah Edison <edison.sarahc@gmail.com>
2021-12-20 21:32:27 -0500

    destructe ImagePopup props

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,21 +1,21 @@
-function ImagePopup(props) {
+function ImagePopup({card, isOpen, onClose}) {
   return (
   
-    <div className={`popup popup_type_${props.card.name} ${props.isOpen && 'popup_opened'}`}> 
+    <div className={`popup popup_type_${card.name} ${isOpen && 'popup_opened'}`}> 
   
       <div className="popup__container popup__container_type_preview">
         <button
           aria-label="close"
           type="button"
           className="popup__close-button popup__close-button_type_preview opacity"
-          onClick={props.onClose}
+          onClick={onClose}
         ></button>
         <img
-          src={props.card.link}
-          alt={props.card.name}
+          src={card.link}
+          alt={card.name}
           className="popup__image"
         />
-        <h2 className="popup__caption">{props.card.name}</h2>
+        <h2 className="popup__caption">{card.name}</h2>
       </div>
     </div>
   );
